f1d4664955b15153a9cdcd0145b53e4dec548456
vaishak.k <vaishak.k@zohocorp.com>
2019-08-16 13:06:01 +0530

    Added Event Handling for child updation in database.

--- scripts/database.js
+++ scripts/database.js
@@ -29,6 +29,11 @@
                 console.log(snap.val());
             });
 
+            self.databaseObject.on('child_changed', snap => {
+                self.updateTodo(snap);
+                console.log(snap.val());
+            });
+
             //     var connectedRef = firebase.database().ref(".info/connected");
             //     connectedRef.on("value", function(snap) {
             //         if (snap.val() === true) {
@@ -41,19 +46,19 @@
 
         addTodo(snap) {
             //handline the addition of todo from database events.
-            var self = this;
             var event = new CustomEvent("child_added", { detail: { snap } });
-
-            // Dispatch/Trigger/Fire the event
             document.dispatchEvent(event);
         },
 
         removeTodo(snap) {
             //handline the removal of todo from database events
-            var self = this;
             var event = new CustomEvent("child_removed", { detail: { snap } });
+            document.dispatchEvent(event);
+        },
 
-            // Dispatch/Trigger/Fire the event
+        updateTodo(snap) {
+            //handling the updation of todo mainly completion toggle.
+            var event = new CustomEvent("child_updated", { detail: { snap } });
             document.dispatchEvent(event);
         },
 
